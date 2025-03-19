a910192a5897a09b3f8407cd78b03cd2a4b855f4
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-19 16:32:16 +0100

    [IMP] :sparkles: add tag_ids in form view

--- models/estate_property.py
+++ models/estate_property.py
@@ -56,3 +56,8 @@ class EstateProperty(models.Model):
         string='Salesperson',
         default=lambda self: self.env.user
     )
+
+    tag_ids = fields.Many2many(
+        'estate.property.tag',
+        string='Tags'
+    )
