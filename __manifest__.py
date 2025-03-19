e56bb71410310b7580f21b2c92418ef52b09ce9c
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-19 10:37:18 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,6 @@
+{
+    'name': 'Real Estate',
+    'depends': ['base'],
+    'data': ['data/ir.model.access.csv', 'data/estate_property_views.xml', 'data/estate_menus.xml'],
+    'application': True
+}
---
+++ models/estate_property.py
@@ -0,0 +1,31 @@
+from odoo import models, fields
+from datetime import timedelta
+
+ORIENTATIONS = [
+    ('north', 'North'),
+    ('south', 'South'),
+    ('east', 'East'),
+    ('west', 'West')
+]
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "An estate property"
+
+    name = fields.Char('Propertys name', required=True)
+    description = fields.Text('Description')
+    postcode = fields.Char('Pstcode')
+    date_availability = fields.Date('Aailability', copy=False, default=fields.Date.today() + timedelta(days=90))
+    expected_price = fields.Float('Excpected price', required=True)
+    selling_price = fields.Float('Selling price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
+    living_area = fields.Integer('Living area')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden area')
+    garden_orientation = fields.Selection(
+        string='Garden orientation',
+        selection=ORIENTATIONS
+    )
+    active = fields.Boolean('Active', default=True)
