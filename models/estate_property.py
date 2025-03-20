5f9832c4dc349d41227f045460ca6e6e91f79d72
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-20 09:30:39 +0100

    [ADD] :sparkles: add total_area computed field

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 from datetime import timedelta
 
 ORIENTATIONS = [
@@ -67,3 +67,10 @@ class EstateProperty(models.Model):
         'property_id',
         string='Offers'
     )
+
+    total_area = fields.Float(compute='_compute_total_area')
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
