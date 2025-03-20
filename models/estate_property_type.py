c58af40b91e72af75579cfb9157e421d364dba23
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-20 13:48:48 +0100

    [ADD] estate.property.type unique name constraint

--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -3,5 +3,8 @@ from odoo import models, fields
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "An estate property type"
+    _sql_constraints = [
+        ('check_name', 'UNIQUE(name)', 'The property type name must be unique !')
+    ]
 
     name = fields.Char('Property type', required=True)
