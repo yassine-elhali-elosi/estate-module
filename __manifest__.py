c2bd63e724115d37020cc5b21cd62d57cc984bd1
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-20 15:48:11 +0100

    [REF] bettr xml views structure

--- __manifest__.py
+++ __manifest__.py
@@ -2,10 +2,8 @@
     'name': 'Real Estate',
     'depends': ['base'],
     'data': [
-        'views/estate_property_actions.xml',
         'views/estate_property_types_actions.xml',
         'views/estate_property_tag_actions.xml',
-        'views/estate_menus.xml',
         'views/estate_property_views.xml',
         'security/ir.model.access.csv',
     ],
