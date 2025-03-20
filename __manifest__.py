152ebd7c1c1310eaf5e42a22b3cfa0cef66aa8d9
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-20 16:01:23 +0100

    [REF] model views rename

--- __manifest__.py
+++ __manifest__.py
@@ -2,9 +2,9 @@
     'name': 'Real Estate',
     'depends': ['base'],
     'data': [
-        'views/estate_property_types_actions.xml',
-        'views/estate_property_tag_actions.xml',
-        'views/estate_property_views.xml',
+        'views/estate_property_view.xml',
+        'views/estate_property_types_view.xml',
+        'views/estate_property_tag_view.xml',
         'security/ir.model.access.csv',
     ],
     'application': True
