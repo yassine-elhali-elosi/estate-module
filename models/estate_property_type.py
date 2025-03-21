82cf0df8887831a1e970dcdcc8cd5e473cf63508
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-21 09:27:07 +0100

    [ADD] sequence field in property types

--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -6,7 +6,8 @@ class EstatePropertyType(models.Model):
     _sql_constraints = [
         ('uniq_name', 'UNIQUE(name)', 'The property type name must be unique !')
     ]
-    _order = "name"
+    _order = "sequence, name"
 
     name = fields.Char('Property type', required=True)
     property_ids = fields.One2many('estate.property', 'property_type_id')
+    sequence = fields.Integer('Sequence', default=1, help='Used to order property types')
