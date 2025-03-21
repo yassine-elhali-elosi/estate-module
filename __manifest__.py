346cf1fbbb0e7b59119eba49f9a196607220b7f0
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-21 14:34:11 +0100

    [FIX] load offers before types

--- __manifest__.py
+++ __manifest__.py
@@ -3,9 +3,9 @@
     'depends': ['base'],
     'data': [
         'views/estate_property_view.xml',
-        'views/estate_property_types_view.xml',
         'views/estate_property_tag_view.xml',
         'views/estate_property_offer_view.xml',
+        'views/estate_property_types_view.xml',
         'security/ir.model.access.csv',
     ],
     'application': True
