7f73df73f40110770eb84bfe1841ec8da2f2b10c
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-24 10:12:17 +0100

    [ADD] generate customer invoice for sold property

--- estate-account/models/estate_property.py
+++ estate-account/models/estate_property.py
@@ -5,4 +5,10 @@ class EstateProperty(models.Model):
 
     def sold_property(self):
         print(">>>>>>>>>>>>>>>test inherited estate_property.sold_property() method")
+        self.env['account.move'].create(
+            {
+                'partner_id': self.buyer.id,
+                'move_type': 'out_invoice'
+            }
+        )
         return super(EstateProperty, self).sold_property()
