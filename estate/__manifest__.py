39de770b504d1d900a68dfc2d48051a46b5338ac
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-24 13:54:54 +0100

    [FIX] load offers before types for relation

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,8 +3,8 @@
     'depends': ['base'],
     'data': [
         'views/property_tag_view.xml',
-        'views/property_types_view.xml',
         'views/property_offer_view.xml',
+        'views/property_types_view.xml',
         'views/property_view.xml',
         'views/res_users_view.xml',
         'views/estate_property_menus.xml',
