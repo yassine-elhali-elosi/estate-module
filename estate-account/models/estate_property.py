ac66d921aa074dfaab200c37e527eb9b1c9b9c26
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-24 10:24:10 +0100

    [IMP] added property title, price and quantity in generated invoice

--- estate-account/models/estate_property.py
+++ estate-account/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models, api
+from odoo import models, Command
 
 class EstateProperty(models.Model):
     _inherit = 'estate.property'
@@ -8,7 +8,14 @@ class EstateProperty(models.Model):
         self.env['account.move'].create(
             {
                 'partner_id': self.buyer.id,
-                'move_type': 'out_invoice'
+                'move_type': 'out_invoice',
+                'invoice_line_ids': [
+                    Command.create({
+                        "name": self.name,
+                        "quantity": 1,
+                        "price_unit": self.selling_price + 100.00 + (self.selling_price * 0.06)
+                    })
+                ]
             }
         )
         return super(EstateProperty, self).sold_property()
