4a0c5300c36edf5c154f3ec64e636f29595ed3c0
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-24 14:22:19 +0100

    [REF] property_type not types

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,11 +4,12 @@
     'data': [
         'views/property_tag_view.xml',
         'views/property_offer_view.xml',
-        'views/property_types_view.xml',
+        'views/property_type_view.xml',
         'views/property_view.xml',
         'views/res_users_view.xml',
         'views/estate_property_menus.xml',
         'security/ir.model.access.csv',
+        'data/property_type_data.csv'
     ],
     'application': True
 }
