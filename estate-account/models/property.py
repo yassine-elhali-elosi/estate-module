d60ac4e5e60241c5159f72baf0be4c251c64ea74
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-24 13:43:34 +0100

    [REF] refactoring views and order in manifest

--- estate-account/models/property.py
+++ estate-account/models/property.py
@@ -4,7 +4,6 @@ class EstateProperty(models.Model):
     _inherit = 'estate.property'
 
     def sold_property(self):
-        print(">>>>>>>>>>>>>>>test inherited estate_property.sold_property() method")
         self.env['account.move'].create(
             {
                 'partner_id': self.buyer.id,
