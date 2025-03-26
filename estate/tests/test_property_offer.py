78767491cb8486feab2340662aaada78d6b4dca5
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-26 10:02:48 +0100

    [ADD] unit test mark a property as sold

--- estate/tests/test_property_offer.py
+++ estate/tests/test_property_offer.py
@@ -10,6 +10,7 @@ class EstatePropertyOfferTestCase(TransactionCase):
         cls.properties = cls.env['estate.property'].create([
             {'name': 'Villa with garden', 'expected_price': 100000, 'state': 'sold'},
             {'name': 'Apartment with view', 'expected_price': 50000, 'state': 'new'},
+            {'name': 'House', 'expected_price': 75000, 'state': 'offer_accepted'},
         ])
 
     def test_create_offer_on_sold_property(self):
@@ -21,6 +22,10 @@ class EstatePropertyOfferTestCase(TransactionCase):
                 'partner_id': self.env.ref('base.res_partner_12').id,
             })
 
-    def test_create_offer_on_property_with_no_accepted_offer(self):
+    def test_mark_property_as_sold_without_accepted_offer(self):
         with self.assertRaises(UserError):
             self.properties[1].sold_property()
+            
+    def test_mark_property_as_sold_with_accepted_offer(self):
+        self.properties[2].sold_property()
+        self.assertEqual(self.properties[2].state, 'sold')
