9b48b97adad4b5f5aa992443ad73f0c85f135ff6
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-26 11:58:51 +0100

    [ADD] start data module view

--- estate-data-module/__manifest__.py
+++ estate-data-module/__manifest__.py
@@ -3,7 +3,8 @@
     'depends': ['base', 'base_import_module'],
     'data': [
         'models/estate_property.xml',
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/property_view.xml'
     ],
     'application': True
 }
