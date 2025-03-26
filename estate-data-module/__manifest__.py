79379fa2958fcc4c0d6d808d32ccad964b07e5ea
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-26 14:04:25 +0100

    [ADD] add property type data model

--- estate-data-module/__manifest__.py
+++ estate-data-module/__manifest__.py
@@ -3,7 +3,10 @@
     'depends': ['base', 'base_import_module'],
     'data': [
         'models/estate_property.xml',
-        'views/property_view.xml',
+        'models/estate_property_type.xml',
+        'views/estate_property_view.xml',
+        'views/estate_property_type_view.xml',
+        'views/estate_property_menus.xml',
         'security/ir.model.access.csv',
     ],
     'application': True
