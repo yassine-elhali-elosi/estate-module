5decd1facc056502d51230770389efa6da5724bc
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-26 11:39:41 +0100

    [FIX] load the model

--- estate-data-module/__manifest__.py
+++ estate-data-module/__manifest__.py
@@ -1,5 +1,9 @@
 {
     'name': 'Estate Data Module',
     'depends': ['base', 'base_import_module'],
+    'data': [
+        'models/estate_property.xml',
+        'security/ir.model.access.csv'
+    ],
     'application': True
 }
