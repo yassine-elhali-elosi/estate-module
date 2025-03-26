59f831a870b1170d21f1b22a197c5ce3a7a8245c
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-26 14:35:43 +0100

    [FIX] load property type data model and view

--- estate-data-module/__manifest__.py
+++ estate-data-module/__manifest__.py
@@ -3,7 +3,9 @@
     'depends': ['base', 'base_import_module'],
     'data': [
         'security/ir.model.access.csv',
+        'models/estate_property_type.xml',
         'models/estate_property.xml',
+        'views/estate_property_type_view.xml',
         'views/estate_property_view.xml',
         'views/estate_property_menus.xml',
     ],
