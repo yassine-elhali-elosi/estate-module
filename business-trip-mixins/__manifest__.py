ffa675453ad0c380bda229d873e4be2a7b0d476f
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-27 14:56:59 +0100

    [ADD] added menu and security to new module for mixins

--- business-trip-mixins/__manifest__.py
+++ business-trip-mixins/__manifest__.py
@@ -2,7 +2,9 @@
     "name": "Business Trip",
     "depends": ["mail"],
     "data": [
-        "views/business_trip_view.xml"
-    ];
+        "views/business_trip_view.xml",
+        "views/business_trip_menus.xml",
+        "security/ir.model.access.csv"
+    ],
     "application": True
 }
