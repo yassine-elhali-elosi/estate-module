22eb08b9dd4a6a187dbd2470acb3882949c155c5
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-27 15:17:31 +0100

    [ADD] added tracking log on name and partner_id

--- business-trip-mixins/models/business_trip.py
+++ business-trip-mixins/models/business_trip.py
@@ -5,6 +5,6 @@ class BusinessTrip(models.Model):
     _inherit = ["mail.thread"]
     _description = "Business Trip"
 
-    name = fields.Char()
-    partner_id = fields.Many2one("res.partner", "Responsible")
+    name = fields.Char(tracking=True)
+    partner_id = fields.Many2one("res.partner", "Responsible", tracking=True)
     guest_ids = fields.Many2many("res.partner", "Participants")
