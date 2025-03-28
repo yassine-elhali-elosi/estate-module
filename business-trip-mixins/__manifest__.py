0bd18b634fe1cbd31bc3fa2a0b8b58917f381a3f
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-28 10:48:25 +0100

    [ADD] create business_expense view for each business_trip

--- business-trip-mixins/__manifest__.py
+++ business-trip-mixins/__manifest__.py
@@ -2,8 +2,9 @@
     "name": "Business Trip",
     "depends": ["mail"],
     "data": [
+        "views/business_expense_view.xml",
         "views/business_trip_view.xml",
-        "views/business_trip_menus.xml",
+        "views/business_menus.xml",
         "security/ir.model.access.csv"
     ],
     "application": True
