0baf72622e38e92fed0aced75d3a4011d6055935
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-28 15:09:28 +0100

    [ADD] xmlrpc call methods in a model

--- odoo_doc_rpc.py
+++ odoo_doc_rpc.py
@@ -7,5 +7,11 @@ password = 'admin'
 
 # 1. Authentification
 common = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/common')
+print(common.version())
 uid = common.authenticate(db, username, password, {})
 print("UID:", uid)
+
+# Call methods in models
+models = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/object')
+results_name_search = models.execute_kw(db, uid, password, 'res.partner', 'name_search', ['Agent Elosi'], {'limit': 10})
+print(results_name_search)
