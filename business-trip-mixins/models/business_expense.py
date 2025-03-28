474c23d61eeae737154c632db7918d1a71ec4018
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-28 10:27:34 +0100

    [ADD] get values from email when expense created

--- business-trip-mixins/models/business_expense.py
+++ business-trip-mixins/models/business_expense.py
@@ -1,3 +1,4 @@
+import re
 from odoo import models, fields
 
 class BusinessExpense(models.Model):
@@ -9,3 +10,22 @@ class BusinessExpense(models.Model):
     amount = fields.Float("Amount")
     trip_id = fields.Many2one("business.trip", "Business Trip")
     partner_id = fields.Many2one("res.partner", "Created by")
+
+    def message_new(self, msg, custom_values=None):
+        name = msg.get("subject", "New Expense")
+        email_from = msg.get("email_from")
+
+        amount_pattern = '(\d+(\.\d*)?|\.\d+)'
+        expense_price = re.findall(amount_pattern, name)
+        price = expense_price and float(expense_price[-1][0]) or 1.0
+
+        partner = self.env["res.partner"].search(["email", "like", email_from], limit=1)
+
+        defaults = {
+            "name": name,
+            "amount": price,
+            "partner_id": partner.id
+        }
+        defaults.update(custom_values or {})
+        res = super().message_new(msg, custom_values=defaults)
+        return res
