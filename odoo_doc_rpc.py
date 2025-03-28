1586eb157a1505441098c107fac68ce6d304dfdf
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-28 15:44:23 +0100

    [ADD] get fields of a model

--- odoo_doc_rpc.py
+++ odoo_doc_rpc.py
@@ -17,16 +17,19 @@ results_name_search = models.execute_kw(db, uid, password, 'res.partner', 'name_
 print("res.partner> name_search(): ", results_name_search)
 
 results_customer_companies = models.execute_kw(db, uid, password, 'res.partner', 'search', [[['is_company', '=', True]]])
-print("res.partner> is_company=True: ", results_customer_companies)
+print("\nres.partner> is_company=True: ", results_customer_companies)
 
 results_customer_companies_with_offset = models.execute_kw(db, uid, password, 'res.partner', 'search', [[['is_company', '=', True]]], {"offset": 5, "limit": 5})
-print("res.partner> is_company=True, offset=10: ", results_customer_companies_with_offset)
+print("\nres.partner> is_company=True, offset=10: ", results_customer_companies_with_offset)
 
 results_customer_companies_count = models.execute_kw(db, uid, password, 'res.partner', 'search_count', [[['is_company', '=', True]]])
-print("res.partner> is_company=True, count: ", results_customer_companies_count)
+print("\nres.partner> is_company=True, count: ", results_customer_companies_count)
 
 results_customer_companies_records = models.execute_kw(db, uid, password, 'res.partner', 'read', [results_customer_companies])
-print("res.partner> is_company=True, number of records: ", len(results_customer_companies_records))
+print("\nres.partner> is_company=True, number of records: ", len(results_customer_companies_records))
 
 results_customer_companies_records_specific_fields = models.execute_kw(db, uid, password, 'res.partner', 'read', [results_customer_companies], {"fields": ["name", "country_id", "comment"]})
-print("res.partner> is_company=True, records(name, country_id, comment): ", results_customer_companies_records_specific_fields)
+print("\nres.partner> is_company=True, records(name, country_id, comment): ", results_customer_companies_records_specific_fields)
+
+results_res_partner_fields = models.execute_kw(db, uid, password, "res.partner", "fields_get", [], {"attributes": ["string", "help", "type"]})
+print("\nres.partner> fields: ", results_res_partner_fields)
