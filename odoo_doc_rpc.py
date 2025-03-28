9017fc19638924d37f1621e10e06a6e6b7791772
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-03-28 15:13:02 +0100

    [ADD] xmlrpc get all customers that are companies

--- odoo_doc_rpc.py
+++ odoo_doc_rpc.py
@@ -14,4 +14,7 @@ print("UID:", uid)
 # Call methods in models
 models = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/object')
 results_name_search = models.execute_kw(db, uid, password, 'res.partner', 'name_search', ['Agent Elosi'], {'limit': 10})
-print(results_name_search)
+print("res.partner> name_search(): ", results_name_search)
+
+results_customer_companies = models.execute_kw(db, uid, password, 'res.partner', 'search', [[['is_company', '=', True]]])
+print("res.partner> is_company=True: ", results_customer_companies)
