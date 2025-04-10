8866e9c6846fcfce5914a947e39b6e64521a3a23
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-04-10 17:10:58 +0200

    start AI for ir.actions.server code

---
+++ elosi-ai/test.py
@@ -0,0 +1,21 @@
+import os
+from mistralai import Mistral
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.environ["MISTRAL_API_KEY"]
+
+client = Mistral(api_key=api_key)
+
+mistral_response = client.agents.complete(
+    agent_id = "ag:7e1f4155:20250410:untitled-agent:a8450e92",
+    messages=[
+        {
+            "role": "user",
+            "content": "I need a server action that adds a tag 'VIP' to all customers with more than 10 sales."
+        }
+    ]
+)
+
+print(mistral_response)
