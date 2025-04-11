3fdd3a2b4d671d6da8b5e3ba74cb3b960747ab60
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-04-11 10:38:42 +0200

    [ADD] computed output

--- elosi-ai/models/elosi_ai.py
+++ elosi-ai/models/elosi_ai.py
@@ -4,7 +4,7 @@ class ElosiAI(models.Model):
     _name = "elosi.ai"
     _description = "Elosi AI for ir.actions.server"
 
-    input_prompt = fields.Text("Input Prompt", required=True)
+    input_prompt = fields.Text("Input Prompt", store=False)
     output_prompt = fields.Text("Output Prompt", compute="_compute_output_prompt", store=True)
 
     @api.depends("input_prompt")
