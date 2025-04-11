033cd1a653f6f16f1f219e6b0400bb2dbfd1fe53
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-04-11 10:51:55 +0200

    [FIX] remove computed + add labels + make output editable

--- elosi-ai/models/elosi_ai.py
+++ elosi-ai/models/elosi_ai.py
@@ -1,24 +1,35 @@
 from odoo import models, fields, api
+import os
+from mistralai import Mistral
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MISTRAL_API_KEY = os.environ["MISTRAL_API_KEY"]
 
 class ElosiAI(models.Model):
     _name = "elosi.ai"
     _description = "Elosi AI for ir.actions.server"
 
     input_prompt = fields.Text("Input Prompt")
-    output_prompt = fields.Text("Output Prompt", compute="_compute_output_prompt", store=True)
-
-    @api.depends("input_prompt")
-    def _compute_output_prompt(self):
-        for record in self:
-            print("input :", record.input_prompt)
-            print("Generating output prompt...")
-            record.output_prompt = "print(\"hello\")"
-            print("Output prompt generated:", record.output_prompt)
+    output_prompt = fields.Text("Output Prompt", store=True)
 
     def generate_code(self):
         for record in self:
             print("Generating code for:", record.input_prompt)
+            client = Mistral(api_key=MISTRAL_API_KEY)
 
-            record.output_prompt = "print(\"hello\")\n" + record.input_prompt
+            mistral_response = client.agents.complete(
+                agent_id = "ag:7e1f4155:20250410:untitled-agent:a8450e92",
+                messages=[
+                    {
+                        "role": "user",
+                        "content": record.input_prompt
+                    }
+                ]
+            )
+
+            generated_code = mistral_response.choices[0].message.content
+            record.output_prompt = generated_code
         
         return True
