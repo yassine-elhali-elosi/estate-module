235104253a4e016758beb3c9858fb24b71da9ea4
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-04-15 14:57:48 +0200

    [REF] start replacing Mistral agent with our LLM

--- elosi-ai/models/elosi_ai.py
+++ elosi-ai/models/elosi_ai.py
@@ -10,8 +10,6 @@ print(llm.TEMPLATE)
 
 load_dotenv()
 
-MISTRAL_API_KEY = os.environ["MISTRAL_API_KEY"]
-
 class ElosiAI(models.Model):
     _name = "elosi.ai"
     _description = "Elosi AI for ir.actions.server"
@@ -24,31 +22,12 @@ class ElosiAI(models.Model):
         self.ensure_one() 
         print("Generating code for:", self.input_prompt)
         self.output_prompt = "Generating code..."
-        client = Mistral(api_key=MISTRAL_API_KEY)
 
-        mistral_response = client.agents.complete(
-            agent_id = "ag:7e1f4155:20250414:untitled-agent:bb0a05f5",
-            messages=[
-                {
-                    "role": "user",
-                    "content": self.input_prompt
-                }
-            ]
-        )
+        llm_response = llm.generate_code(self.input_prompt)
 
-        print("Response from Mistral:", mistral_response)
+        print("Response from LLM:", llm_response)
 
-        generated_code = mistral_response.choices[0].message.content
-
-        #generated_code = "print(\"hello\")\n" + (self.input_prompt)
-        self.output_prompt = generated_code
-
-        print("Generated code:", generated_code)
-        
-        local_vars = {'self': self}
-        exec(f"result = {generated_code}", local_vars)
-        self.output_result = local_vars.get('result')
-        print("Result:", self.output_result)
+        self.output_prompt = llm_response
 
         self.input_prompt = ""
         
