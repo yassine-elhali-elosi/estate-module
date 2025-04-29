32cd52a6f5de3ebc1af683ae1f0f1209c9e5ce5d
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-04-29 15:55:40 +0200

    [IMP] changed model to codegen-350M-mono

--- elosi-ai/llm/main.py
+++ elosi-ai/llm/main.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(current_dir, "fine-tuned-odoo-model")
+model_path = os.path.join(current_dir, "fine-tuned-codegen")
 
 tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
 model = AutoModelForCausalLM.from_pretrained(model_path, local_files_only=True)
@@ -13,7 +13,6 @@ def generate_code(prompt):
 
     inputs = tokenizer(input_text, return_tensors="pt")
     outputs = model.generate(inputs.input_ids, max_length=200)
-    print("###################")
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
     print(response)
