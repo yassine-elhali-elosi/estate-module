607f1f2e9a15f2476ae7bdb84854014c32046170
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-04-30 10:58:34 +0200

    [ADD] transfered model training locally

---
+++ elosi-ai/llm/codegen-350M-mono/main-codegen.py
@@ -0,0 +1,21 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# ancien model, distilgpt2
+#model_path = "./fine_tuned_odoo_model"
+
+model_path = "./fine-tuned-codegen-350M-mono"
+
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(model_path)
+
+input_text = """### input:
+How many delivery orders are there in the system?
+
+### output:"""
+
+input_ids = tokenizer(input_text, return_tensors="pt")
+generated_ids = model.generate(input_ids=input_ids.input_ids, attention_mask=input_ids.attention_mask, max_new_tokens=300)
+
+generated_text = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+
+print(generated_text)
---
+++ elosi-ai/llm/codegen-350M-mono/simple_fine_tune.py
@@ -0,0 +1,70 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer
+
+dataset = load_dataset("imsanjoykb/orm-odoo-dataset-30-v2")
+print("Dataset loaded")
+
+model_name = "Salesforce/codegen-350M-mono"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+# le padding token de fin
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.eos_token
+
+# conversion du dataset en token pour le model
+def format_and_tokenize(examples):
+    formatted_texts = []
+
+    for i in range(len(examples['input'])):
+        text = (
+            f"### input:\n{examples['input'][i]}\n"
+            f"### output:\n{examples['output'][i]}\n\n"
+        )
+        formatted_texts.append(text)
+
+    #print(formatted_texts)
+
+    encoded = tokenizer(
+        formatted_texts,
+        padding="max_length",
+        max_length=512,
+        truncation=True,
+        return_tensors="pt"
+    )
+
+    # For training, the labels are the same as the inputs
+    encoded["labels"] = encoded["input_ids"].clone()
+
+    return encoded
+
+tokenized_dataset = dataset.map(
+    format_and_tokenize,
+    batched=True,
+    remove_columns=dataset["train"].column_names
+)
+
+# le training
+output_dir = "./fine-tuned-codegen-350M-mono"
+
+training_args = TrainingArguments(
+    output_dir=output_dir,
+    num_train_epochs=5,
+    per_device_train_batch_size=4,
+    warmup_steps=100,
+    #weight_decay=0.01,
+    save_strategy="epoch"
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_dataset["train"],
+)
+
+trainer.train()
+print("Training complete")
+
+model.save_pretrained(output_dir)
+tokenizer.save_pretrained(output_dir)
+print("New model saved")
