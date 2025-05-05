05e33f2b29a2ca1a92983c3358032e6865c60fd3
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-05-05 11:38:13 +0200

    [IMP] improved + added system prompt

--- elosi-ai/llm/codegen-350M-mono/simple_fine_tune.py
+++ elosi-ai/llm/codegen-350M-mono/simple_fine_tune.py
@@ -1,8 +1,13 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer
 
+from datasets import load_from_disk
+
 dataset = load_dataset("imsanjoykb/orm-odoo-dataset-30-v2")
 print("Dataset loaded")
+
+# dataset = load_from_disk('ir_actions_server_dataset')
+# print("Custom ir.actions.server dataset loaded")
 
 model_name = "Salesforce/codegen-350M-mono"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -35,31 +40,35 @@ def format_and_tokenize(examples):
 
     # For training, the labels are the same as the inputs
     encoded["labels"] = encoded["input_ids"].clone()
-
+    
     return encoded
 
 tokenized_dataset = dataset.map(
     format_and_tokenize,
     batched=True,
-    remove_columns=dataset["train"].column_names
+    remove_columns=dataset.column_names  # Changed from dataset["train"].column_names
 )
+
+# Create train_test split
+dataset_dict = tokenized_dataset.train_test_split(test_size=0.1)
 
 # le training
 output_dir = "./fine-tuned-codegen-350M-mono"
+# output_dir = "./fine-tuned-codegen-350M-mono-actions-server"
 
 training_args = TrainingArguments(
     output_dir=output_dir,
     num_train_epochs=5,
     per_device_train_batch_size=4,
     warmup_steps=100,
-    #weight_decay=0.01,
-    save_strategy="epoch"
+    save_strategy="epoch",
 )
 
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=tokenized_dataset["train"],
+    train_dataset=dataset_dict['train'],
+    eval_dataset=dataset_dict['test'],
 )
 
 trainer.train()
