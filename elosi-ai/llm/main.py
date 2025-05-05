fee6dbd8fa684fead6ec94ec0a2da60380b27eb5
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-05-05 11:36:15 +0200

    [FIX] better formating

--- elosi-ai/llm/main.py
+++ elosi-ai/llm/main.py
@@ -2,17 +2,20 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(current_dir, "fine-tuned-codegen")
+model_path = os.path.join(current_dir, "codegen-350M-mono/fine-tuned-codegen-350M-mono")
 
 tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
 model = AutoModelForCausalLM.from_pretrained(model_path, local_files_only=True)
 
 def generate_code(prompt):
     #prompt = "How many delivery orders are there in the system?"
-    input_text = f"### Input:\n{prompt}\n### Instruction:"
+    input_text = f"""### input:
+    {prompt}
+    
+    ### output:"""
 
     inputs = tokenizer(input_text, return_tensors="pt")
-    outputs = model.generate(inputs.input_ids, max_length=200)
+    outputs = model.generate(inputs.input_ids, max_length=500)
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
     print(response)
